cbbe967cf1b1f199624d7aefdcfd8c861c9a0503
Chijioke <wisestcodes@gmail.com>
2023-08-01 19:39:39 -0400

    toggle form content

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent;
+
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     function (position) {
@@ -21,7 +23,7 @@ if (navigator.geolocation) {
 
       const coords = [latitude, longitude];
 
-      const map = L.map('map').setView(coords, 13);
+      map = L.map('map').setView(coords, 13);
 
       L.tileLayer('https://tileserver.memomaps.de/tilegen/{z}/{x}/{y}.png', {
         attribution:
@@ -33,23 +35,11 @@ if (navigator.geolocation) {
       //     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
       //     .openPopup();
 
-      map.on('click', function (mapEvent) {
-        const { lat, lng } = mapEvent.latlng;
-
-        L.marker([lat, lng])
-          .addTo(map)
-          .bindPopup(
-            L.popup({
-              maxWidth: 250,
-              minWidth: 100,
-              autoClose: false,
-              closeOnClick: false,
-              className: 'running-popup',
-            })
-          )
-          .setPopupContent('Fun workout')
-          .openPopup();
-        console.log(mapEvent);
+      //   handle clicks on map
+      map.on('click', function (mapE) {
+        mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
       });
     },
     function () {
@@ -57,3 +47,30 @@ if (navigator.geolocation) {
     }
   );
 }
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  form.reset();
+
+  // display marker
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('Fun workout')
+    .openPopup();
+  console.log(mapEvent);
+});
+
+inputType.addEventListener('change', function () {
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+});
